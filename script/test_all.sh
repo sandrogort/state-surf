29fd25abe90687d28d45f6edbd356c1e002a9cce
Sandro Gort <sandro.gort@gmail.com>
2025-10-03 18:12:34 +0200

    Test script

---
+++ script/test_all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+"${SCRIPT_DIR}/generate.sh"
+"${SCRIPT_DIR}/build_cpp.sh"
+"${SCRIPT_DIR}/run_cpp_test.sh"
+"${SCRIPT_DIR}/build_rust.sh"
+"${SCRIPT_DIR}/run_rust_test.sh"
