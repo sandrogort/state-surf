057340b2c0437c5abe8abb65e2966752c278eaf3
Sandro Gort <sandro.gort@gmail.com>
2025-11-02 13:57:13 +0100

    Added cpp static analysis to test_all script

--- script/test_all.sh
+++ script/test_all.sh
@@ -4,8 +4,13 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 "${SCRIPT_DIR}/generate.sh"
+
 "${SCRIPT_DIR}/build_cpp.sh"
 "${SCRIPT_DIR}/run_cpp_test.sh"
+"${SCRIPT_DIR}/run-clang-tidy.sh"
+"${SCRIPT_DIR}/run-cppcheck.sh"
+
 "${SCRIPT_DIR}/build_rust.sh"
 "${SCRIPT_DIR}/run_rust_test.sh"
+
 "${SCRIPT_DIR}/run_python_test.sh"
